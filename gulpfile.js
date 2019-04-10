1eb4f669694a17be29acf764d62dbd991eaf4146
Max Dragaytsev <dmax1447@gmail.com>
2019-04-10 19:10:37 +0300

    сделал верстку крупных блоков

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@ var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var postcssCustomProperties = require('postcss-custom-properties');
+var postcssCustomMedia = require('postcss-custom-media');
+var sorting = require('postcss-sorting');
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var typograf = require('gulp-typograf');
 
 
 gulp.task("css", function () {
@@ -16,9 +19,11 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      postcssCustomMedia(),
+      postcssCustomProperties(),
+      sorting()
     ]))
-    .pipe(postcss([postcssCustomProperties()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
@@ -38,3 +43,9 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("typograf", function() {
+  gulp.src("source/*.html")
+    .pipe(typograf({ locale: ["ru", "en-US"] }))
+    .pipe(gulp.dest("source/*.html"));
+});
